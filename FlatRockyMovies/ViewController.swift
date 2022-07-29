68ea9e0590c92a87779684a1b204d86ed443aa15
Gaga <galaqtionn@gmail.com>
2022-07-29 20:59:15 +0400

    Initial Commit

---
+++ FlatRockyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlatRockyMovies
+//
+//  Created by Gaga Nizharadze on 29.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
